b4e509c09f2148c672227ec84086614f3a6c9183
Justin Carruthers <jtcruthers@gmail.com>
2019-06-17 00:54:38 -0400

    Bouncing rectangle

---
+++ quicksilver/src/main.rs
@@ -0,0 +1,67 @@
+extern crate quicksilver;
+extern crate rand;
+
+use rand::Rng;
+
+use quicksilver::{
+    Result,
+    geom::{Rectangle, Transform, Vector},
+    graphics::{Background, Color},
+    lifecycle::{Settings, State, Window, run},
+};
+
+struct Screen {
+    position: Vector,
+    velocity: Vector
+}
+
+impl Screen {
+    fn set_random_position(&mut self) {
+        let mut rng = rand::thread_rng();
+        let x = rng.gen_range::<f32>(0.0, 1600.0);
+        let y = rng.gen_range::<f32>(0.0, 1200.0);
+        self.position.x = x;
+        self.position.y = y;
+    }
+
+    fn slide(&mut self) {
+        if self.position.x > 1600.0 || self.position.x < 0.0 {
+            self.velocity.x *= -1.0;
+        }
+        if self.position.y > 1200.0 || self.position.y < 0.0 {
+            self.velocity.y *= -1.0;
+        }
+        self.position.x += self.velocity.x;
+        self.position.y += self.velocity.y;
+    }
+}
+
+impl State for Screen {
+    fn new() -> Result<Screen> {
+        Ok(Screen {
+            position: Vector::new(50, 50),
+            velocity: Vector::new(5, 5)
+        })
+    }
+
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
+        window.clear(Color::WHITE)?;
+
+        self.slide();
+        println!("position {}\nvelocity {}", self.position, self.velocity);
+        window.draw_ex(
+            &Rectangle::new(self.position, (100, 100)),
+            Background::Col(Color::BLUE),
+            Transform::rotate(45),
+            0
+        );
+        Ok(())
+    }
+}
+
+fn main() {
+    run::<Screen>("Rectangluar", Vector::new(1600, 1200), Settings {
+        icon_path: Some("image.png"),
+        ..Settings::default()
+    });
+}
