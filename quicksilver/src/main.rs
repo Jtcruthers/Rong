aaecf4be8a690c5548e85e483dd6696d54fee1bc
Justin Carruthers <jtcruthers@gmail.com>
2019-06-17 01:04:21 -0400

    Added basic input

--- quicksilver/src/main.rs
+++ quicksilver/src/main.rs
@@ -7,6 +7,7 @@ use quicksilver::{
     Result,
     geom::{Rectangle, Transform, Vector},
     graphics::{Background, Color},
+    input::Key,
     lifecycle::{Settings, State, Window, run},
 };
 
@@ -40,14 +41,35 @@ impl State for Screen {
     fn new() -> Result<Screen> {
         Ok(Screen {
             position: Vector::new(50, 50),
-            velocity: Vector::new(5, 5)
+            velocity: Vector::new(0, 0)
         })
+    }
+
+    fn update(&mut self, window: &mut Window) -> Result<()> {
+        if window.keyboard()[Key::Right].is_down() {
+            self.velocity.x += 0.1;
+        }
+        if window.keyboard()[Key::Left].is_down() {
+            self.velocity.x -= 0.1;
+        }
+        if window.keyboard()[Key::Up].is_down() {
+            self.velocity.y -= 0.1;
+        }
+        if window.keyboard()[Key::Down].is_down() {
+            self.velocity.y += 0.1;
+        }
+        if window.keyboard()[Key::Space].is_down() {
+            self.velocity.x = 0.0;
+            self.velocity.y = 0.0;
+        }
+        self.position.x += self.velocity.x;
+        self.position.y += self.velocity.y;
+        Ok(())
     }
 
     fn draw(&mut self, window: &mut Window) -> Result<()> {
         window.clear(Color::WHITE)?;
 
-        self.slide();
         println!("position {}\nvelocity {}", self.position, self.velocity);
         window.draw_ex(
             &Rectangle::new(self.position, (100, 100)),
