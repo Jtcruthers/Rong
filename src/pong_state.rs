d3421501d1b0b43b77c8097deb393ed6b29d6bf6
Justin Carruthers <jtcruthers@gmail.com>
2019-06-23 21:34:30 -0400

    Separated draw method into logical methods

--- src/pong_state.rs
+++ src/pong_state.rs
@@ -84,25 +84,31 @@ impl State for PongState {
 
     fn draw(&mut self, window: &mut Window) -> Result<()> {
         window.clear(Color::BLACK)?;
-        self.background.execute(|image| {
-            window.draw_ex(
-                &image.area().with_center((960, 540)),
-                Background::Img(&image),
-                Transform::scale((1.0, 1.0)),
-                0
-            );
-            Ok(())
-        }).expect("Could not load background.");
+
+        self.draw_background(window, 0);
+        self.draw_score(window, 1);
+        self.draw_players(window, 2);
+        self.draw_ball(window, 2);
+        
+        Ok(())
+    }
+}
+
+impl PongState {
+    fn draw_ball(&mut self, window: &mut Window, z_index: u8) {
         let position = self.ball.get_position().clone(); //Had to clone it because it complained about borrowing. Look into that
         self.ball.get_background().execute(|image| {
             window.draw_ex(
                 &image.area().with_center(position),
                 Background::Img(&image),
                 Transform::scale((0.05, 0.05)),
-                2
+                z_index
             );
             Ok(())
         }).expect("Could not load ball background.");
+    }
+
+    fn draw_score(&mut self, window: &mut Window, z_index: u8) {
         let cloned_font = self.font.clone();
         cloned_font.borrow_mut().execute(|font| {
             let score1_texture = font.render(&format!("{:02}", self.score.score1), &self.p1_font_style).unwrap();
@@ -119,7 +125,7 @@ impl State for PongState {
                 &image.area().with_center((400, 100)),
                 Background::Img(&image),
                 Transform::scale((3.0, 3.0)),
-                1
+                z_index
             );
         }
         if let Some(ref image) = self.score.score2_texture {
@@ -127,21 +133,35 @@ impl State for PongState {
                 &image.area().with_center((1450, 100)),
                 Background::Img(&image),
                 Transform::scale((3.0, 3.0)),
-                1
+                z_index
             );
         }
+    }
+
+    fn draw_players(&mut self, window: &mut Window, z_index: u8) {
         window.draw_ex(
             &Rectangle::new(self.player_1.position, self.player_1.width),
             Background::Col(self.player_1.background),
             Transform::scale((1.0, 1.0)),
-            1
+            z_index
         );
         window.draw_ex(
             &Rectangle::new(self.player_2.position, self.player_2.width),
             Background::Col(self.player_2.background),
             Transform::scale((1.0, 1.0)),
-            1
+            z_index
         );
-        Ok(())
+    }
+
+    fn draw_background(&mut self, window: &mut Window, z_index: u8) {
+        self.background.execute(|image| {
+            window.draw_ex(
+                &image.area().with_center((960, 540)),
+                Background::Img(&image),
+                Transform::scale((1.0, 1.0)),
+                z_index
+            );
+            Ok(())
+        }).expect("Could not load background.");
     }
 }
